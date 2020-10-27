42d45b396b1a488cf2756e1060856a97adfe4ef0
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2020-10-27 17:01:43 -0400

    Ui changes resources

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,13 +4,17 @@ import footerStyles from "./footer.module.scss"
 
 const Footer = () => {
   const [url, setUrl] = React.useState("")
+  const [language, setLanguage] = React.useState("en")
 
   React.useEffect(() => {
+    if (localStorage.getItem("language") != "") {
+      setLanguage(localStorage.getItem("language"))
+    }
     setUrl(window.location.pathname)
   }, [])
 
   const getFooterText = () => {
-    if(window.location.pathname === "/")
+    if(url === "/")
       return(          
         <p>PRISM-ACCESS Open Minds is a health services innovation project to improve access to mental health care for youth. It is managed by the Youth Mental Health and Technology (YMHTech) Lab at the University of Montreal Hospital Centre, 
         and led by the Canada Research Chair in Innovation and Technology for Youth Mental Health Services |
@@ -19,7 +23,7 @@ const Footer = () => {
         <br/>
         <a href="www.ymhtech.com">www.ymhtech.com</a> / <a href="www.smjtechno.com">www.smjtechno.com</a></p>
       )
-    else if(window.location.pathname !== "/" && localStorage.getItem("language") === "fr"){
+    else if(url !== "/" && language === "fr"){
       return(          
         <p>PRISM-ACCESS Esprits ouverts est un projet d’innovation dans les services de santé visant à améliorer l’accès des jeunes aux soins de santé mentale. Il est géré par le laboratoire Santé mentale des jeunes et technologies (SMJ-techno) 
         du Centre hospitalier de l'Université de Montréal est dirigé par la Chaire de recherche du Canada sur l’innovation et les technologies pour les services de soins de santé mentale pour les jeunes 
@@ -27,7 +31,7 @@ const Footer = () => {
         <a href="www.ymhtech.com">www.ymhtech.com</a> / <a href="www.smjtechno.com">www.smjtechno.com</a></p>
       )
     }
-    else if(window.location.pathname !== "/" && localStorage.getItem("language") === "en" ) {
+    else if(url !== "/" && language === "en" ) {
       return(          
         <p>PRISM-ACCESS Open Minds is a health services innovation project to improve access to mental health care for youth. It is managed by the Youth Mental Health and Technology (YMHTech) Lab at the University of Montreal Hospital Centre, 
         and led by the Canada Research Chair in Innovation and Technology for Youth Mental Health Services
