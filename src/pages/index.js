e63d75eed7014d3319c83364a51a89769200b501
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2020-11-16 16:41:57 -0500

    Main domain branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,97 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/homeLayout"
-
-import headStyle from "../components/head.module.scss"
+import MainLayout from "../components/mainLayout"
 import layoutStyles from "../components/layout.module.scss"
 
+/* Translation using lingui */
+import { I18nProvider } from "@lingui/react"
+import catalogEn from "../locales/en/messages"
+import catalogFr from "../locales/fr/messages"
+import { Trans } from "@lingui/macro"
 
-const IndexPage = () => {
+const IndexPage = ({ location }) => {
+  const [language, setLanguage] = React.useState("en")
+
   React.useEffect(() => {
-    localStorage.removeItem("language")
-  })
+    if (localStorage.getItem("language") !== "") {
+      setLanguage(localStorage.getItem("language"))
+    }
+  }, [])
 
   return (
-    <Layout>
-      <h1>WELCOME | BIENVENUE</h1>
-      <div className={layoutStyles.homeButtons}>
-        <Link
-          to="/welcome"
-          state={{ language: "en" }}
-          onClick={() => {
-            localStorage.setItem("language", "en")
-          }}
-        >
-          <button>ENGLISH</button>
-        </Link>
-        <Link
-          to="/welcome"
-          state={{ language: "fr" }}
-          onClick={() => {
-            localStorage.setItem("language", "fr")
-          }}
-        >
-          <button>FRANÇAIS</button>
-        </Link>
-      </div>
-      <div className={headStyle.headContainer}>
-        <div className={headStyle.headImage}>
-          <img src={"/head_with_text.png"} alt="Head" className={headStyle.logohead} />
+    <I18nProvider
+      language={language}
+      catalogs={{
+        en: catalogEn,
+        fr: catalogFr,
+      }}
+    >
+      <MainLayout index={false} language={language}>
+        <div className={layoutStyles.termOfUse}>
+          <h2>
+            <Trans id="msg.pathway_for_rapid">
+            Pathway for Rapid, Internet-based, Self-referral to Mental health services for youth
+            </Trans>
+          </h2>
+
+          <h1 style={{ textAlign: "center"}}>
+            <Trans id="msg.about_the_prism_access_open_minds_project">ABOUT THE PRISM-ACCESS OPEN MINDS PROJECT</Trans>
+          </h1>
+          <h3 className={layoutStyles.blueStrong}>
+            <Trans id="msg.project_summary">Project Summary</Trans>
+          </h3>
+          <p>
+            <Trans id="msg.main_page_p1">
+            PRISM-ACCESS Open Minds is a services research project conducted in collaboration with the ACCESS Open Minds pan-Canadian network for youth mental health service transformation, community organizations (e.g., Kids Help Phone, mindyourmind), and Canadian-based health technology companies.
+            </Trans>
+          </p>
+          <p>
+            <Trans id="msg.main_page_p2">
+            PRISM-AOM aims to develop, implement, and evaluate the use of an online self-referral pathway to facilitate rapid and direct access to mental health services for youth.
+            </Trans>
+          </p>
+          <p>
+            <Trans id="msg.main_page_p3">
+            Through this pathway, youth with mental health concerns will be able to connect with, and refer themselves directly to a local team specializing in youth mental health care using their phone, computer, or tablet. Parents and service providers will also be able to refer a young person to mental health services via PRISM-AOM. Clinicians will be able to triage, track, and manage referrals through a secured platform.
+            </Trans>
+          </p>
+          <p>
+            <strong className={layoutStyles.blueStrongThin}><Trans id="msg.main_page_p4_intro">PRISM-AOM is not an emergency service.</Trans></strong>
+            {" "}
+            <Trans id="msg.main_page_p4">
+            If you are in a crisis, please call 9-1-1 or contact one of the services listed on the right side of this page.
+            </Trans>
+          </p>
+          <p>
+            <Trans id="msg.main_page_p5">
+            Key partners include: ACCESS Open Minds, Strata Health Solutions, Youth and Family representatives, Tickit Health, Savoir-faire Linux, Kids Help Phone, Wisdom2Action, and mindyourmind.
+            </Trans>
+          </p>
+          <h3 className={layoutStyles.blueStrong}>
+            <Trans id="msg.project_updates">Project Updates</Trans>
+          </h3>
+          <p>
+            <Trans id="msg.main_page_p6">
+            The project has gone live! Click the link below if you would like to access the referral form.
+            </Trans>
+          </p>
+          <p>
+            <Trans id="msg.main_page_p7">
+            *Please note that this is a new referral service that is being piloted for youth 11 to 25 years old in Canada living in Downtown Montreal, Quebec (for youth in homeless situations) and Chatham Kent, Ontario.
+            </Trans>
+          </p>
         </div>
-      </div>
-    </Layout>
+
+        <div className={layoutStyles.welcome}>
+            <a href="https://aom.prismref.ca" target="_blank">
+              <button className={layoutStyles.mainButton}>
+                <Trans id="msg.click_here_for_the_form">CLICK HERE FOR THE FORM</Trans>
+              </button>
+            </a>
+        </div>
+      </MainLayout>
+    </I18nProvider>
   )
 }
 
